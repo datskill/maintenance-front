26bbadca89ef40505f313df9a3db44b90e54951f
hugo thomas <thomas-hugo@orange.fr>
2020-02-26 09:58:15 +0100

    du style

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,12 +3,14 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import './Navbar.css'
+import parisLogo from '../../assets/ville-paris.png'
 
 export default function ButtonAppBar() {
     return (
         <div className="root">
             <AppBar position="static">
                 <Toolbar>
+                    <img className="image-gabarit" src={parisLogo} alt=""></img>
                     <Typography variant="h6" className="title-centred">OpenData Paris</Typography>
                 </Toolbar>
             </AppBar>
--- src/components/input-csv/InputCsv.tsx
+++ src/components/input-csv/InputCsv.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Typography, Grid, GridSpacing, Button } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 import { fileListToArray } from '../../core/utils';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import SaveIcon from '@material-ui/icons/Save';
 import './InputCsv.css'
+
 
 export interface IState {
     files: File[];
@@ -18,52 +21,93 @@ export default class InputCsv extends React.Component<{}, IState> {
         const filesArray = fileListToArray(files);
         if (files) this.setState({ files: filesArray });
     }
+    maintenanceSite() {
+        return (
+            <Grid
+                container
+                direction="column"
+                justify="space-between"
+                alignItems="center"
+                className="maintenance-site">
+                <Grid item md={12}><p className="style-text">Le site est actuellement en maintenance</p></Grid>
+            </Grid>
+
+        );
+    }
+    NoMaintenanceSite() {
+        return (
+            <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                className=""
+            >
+                <Grid item md={12}>
+                    <p className="styled-p">Déposer un fichier CSV</p>
+                </Grid>
+                <Grid className="" item md={12}>
+                    <input
+                        accept=".csv"
+                        id="contained-button-file"
+                        multiple
+                        type="file"
+                        className="display-nothing"
+                        onChange={this.getUploadedFileName}
+                        disabled={this.state.onMaintenance}
+                    />
+                    <label htmlFor="contained-button-file">
+                        <Button variant="contained" color="primary" component="span" startIcon={<SaveIcon />}>
+                            Déposer un fichier
+                            </Button>
+                    </label>
+                </Grid>
+                <Grid className="" item md={12}>
+                    <p> Les fichiers suivants sont importés : </p>
+                    <ul>
+                        {this.state.files ? this.state.files.map(file => {
+                            return <li className="styled-p" key={file.name}>{file.name}</li>
+                        }) : "Aucun fichier importé"}
+                    </ul>
+                </Grid>
+                {console.warn(this.state.files)}
+                {this.state.files.length === 0 ? this.disabledButton() : this.activatedButton()}
+                {console.warn(this.state.files)}
+            </Grid>
+        );
+    }
+    disabledButton() {
+        return (
+            <Grid className="padding-button" item md={12}>
+                <Button
+                    variant="contained"
+                    component="label"
+                    disabled
+                >
+                    Envoyer
+                </Button>
+            </Grid>
+        );
+    }
+    activatedButton() {
+        return (
+            <Grid className="padding-button" item md={12}>
+                <Button
+                    variant="contained"
+                    component="label"
+                    className="custom-button"
+                    startIcon={<CloudUploadIcon />}
+                >
+                    Envoyer
+                </Button>
+            </Grid>
+        );
+    }
+
     render() {
         return (
             <div className="root" >
-                <Grid
-                    container
-                    direction="column"
-                    justify="space-between"
-                    alignItems="center"
-                    className="awesome-padding"
-                >
-                    <Grid item md={12}>
-                        <p>Déposer un fichier CSV</p>
-                    </Grid>
-                    <Grid className="awesome-padding" item md={12}>
-                        <input
-                            accept=".csv"
-                            id="contained-button-file"
-                            multiple
-                            type="file"
-                            className="display-nothing"
-                            onChange={this.getUploadedFileName}
-                            disabled={this.state.onMaintenance}
-                        />
-                        <label htmlFor="contained-button-file">
-                            <Button variant="contained" color="primary" component="span">
-                                Upload
-                            </Button>
-                        </label>
-                    </Grid>
-                    <Grid className="awesome-padding" item md={12}>
-                        <Button
-                            variant="contained"
-                            component="label"
-                        >
-                            Envoyer
-                    </Button>
-                    </Grid>
-                    <Grid className="awesome-padding" item md={12}>
-                        <p> Les fichiers suivants sont importés : </p>
-                        <ul>
-                            {this.state.files ? this.state.files.map(file => {
-                                return <li key={file.name}>{file.name}</li>
-                            }) : "Aucun fichier importé"}
-                        </ul>
-                    </Grid>
-                </Grid>
+                {!this.state.onMaintenance ? this.NoMaintenanceSite() : this.maintenanceSite()}
             </div>
         );
     }
