bd88407636b647f94b3c038beefaf80ec82b6473
AntoineTohan <antoine.durand2@epsi.fr>
2020-03-21 16:23:24 +0100

    [FIX] test jest for update CI workflow

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,8 +1,15 @@
 import Navbar from "./Navbar";
 import * as React from "react";
 import * as renderer from "react-test-renderer";
+import { BrowserRouter as Router } from "react-router-dom";
 
 it("App renders correctly", () => {
-  const tree = renderer.create(<Navbar />).toJSON();
+  const tree = renderer
+    .create(
+      <Router>
+        <Navbar />
+      </Router>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/upload/GeneralPage.test.tsx
+++ src/components/upload/GeneralPage.test.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as renderer from "react-test-renderer";
 import GeneralPage from "./GeneralPage";
+require('jest-fetch-mock').enableMocks()
 
 it("GeneralPage renders correctly", () => {
   const tree = renderer.create(<GeneralPage />).toJSON();
